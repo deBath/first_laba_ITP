0589e675307196d54db3d7f4f80d4eb0210a2b09
Alexey Solovov <alex.solovov@list.ru>
2023-09-14 22:24:29 +0300

    Second version

--- first_laba_ITP/src/code.java
+++ first_laba_ITP/src/code.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class code {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите своё имя: ");
+        String name = scan.nextLine();
+        System.out.print("Приветствуем, " + name + "!");
     }
 }
